21138176df1741530abd655c6aec93291e3298f9
oleheggdal <ole.johnny.heggdal@nav.no>
2021-11-04 11:12:25 +0100

    commiy

--- src/oppgave_3.ps1
+++
@@ -1,4 +0,0 @@
-$date = Read-Host "What is today's date"
-$name = Read-Host "Please enter your name"
-Write-Output "Today's date is $date."
-Write-Output "Today is the day $name began their PowerShell programming journey."
